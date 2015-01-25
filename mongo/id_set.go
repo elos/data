0569e20ba6a35366119c6844dd4038863dc9ff82
Nick Landolfi <nclandolfi@gmail.com>
2015-01-25 02:45:13 -0800

    add bson.ObjectId IDSet, Add(), Drop(), Index()

---
+++ mongo/id_set.go
@@ -0,0 +1,40 @@
+package mongo
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type IDSet []bson.ObjectId
+
+func AddID(s IDSet, id bson.ObjectId) IDSet {
+	_, ok := s.IndexID(id)
+
+	if !ok {
+		ns := append(s, id)
+		return ns
+	}
+
+	return s
+}
+
+func DropID(s IDSet, id bson.ObjectId) IDSet {
+	i, ok := s.IndexID(id)
+
+	if !ok {
+		return s
+	}
+
+	s = s[:i+copy(s[i:], s[i+1:])]
+
+	return s
+}
+
+func (s IDSet) IndexID(id bson.ObjectId) (int, bool) {
+	for i, id := range s {
+		if id == id {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
