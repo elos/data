8f3802938ef9b7a16f3bca576e9865d8a964532c
Nick Landolfi <nclandolfi@gmail.com>
2015-02-08 23:47:04 -0800

    extract example model from store_test

---
+++ example.go
@@ -0,0 +1,87 @@
+package data
+
+import "time"
+
+var ExampleDBType DBType = "example"
+var ExampleKind Kind = "example"
+
+type EM struct {
+	Hello string
+	World int
+	*RecorderID
+}
+
+func NewExampleModel() *EM {
+	return &EM{
+		RecorderID: NewRecorderID("example"),
+	}
+}
+
+// Model Constructor
+func NewEM(s Store) (Model, error) {
+	return NewExampleModel(), nil
+}
+
+func (em *EM) DBType() DBType {
+	return ExampleDBType
+}
+
+func (em *EM) Kind() Kind {
+	return ExampleKind
+}
+
+func (em *EM) ID() ID {
+	return em.RecorderID
+}
+
+func (em *EM) Version() int {
+	return 0
+}
+
+func (em *EM) Valid() bool {
+	return true
+}
+
+func (em *EM) Concerned() []ID {
+	return make([]ID, 0)
+}
+
+func (em *EM) SetID(ID) {
+}
+
+var exampleCanRead = func() bool { return true }
+var exampleCanWrite = func() bool { return true }
+
+func (em *EM) CanRead(c Client) bool {
+	return exampleCanRead()
+}
+
+func (em *EM) CanWrite(c Client) bool {
+	return exampleCanWrite()
+}
+
+func (em *EM) Link(m Model, l Link) error {
+	return nil
+}
+
+func (em *EM) Unlink(m Model, l Link) error {
+	return nil
+}
+
+func (em *EM) SetCreatedAt(t time.Time) {
+}
+
+func (em *EM) SetUpdatedAt(t time.Time) {
+}
+
+func (em *EM) UpdatedAt() time.Time {
+	return time.Now()
+}
+
+func (em *EM) CreatedAt() time.Time {
+	return time.Now()
+}
+
+func (em *EM) Schema() Schema {
+	return NewNullSchema()
+}
