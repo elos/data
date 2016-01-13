d9e6bb82768c1306972976e584428e504cfb0284
Nick Landolfi <nclandolfi@gmail.com>
2016-01-13 14:32:07 -0800

    improve errors.go comments

--- errors.go
+++ errors.go
@@ -1,29 +1,44 @@
 package data
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
+// formatError prepends a package identifier to all error messages
 func formatError(s string) error {
-	return errors.New(fmt.Sprintf("data error: %s", s))
+	return fmt.Errorf("data Error: %s", s)
 }
 
 var (
-	// ErrNotFound is when PopulateByID or PopulateByField
-	// could not be fulfilled
+	// ErrNotFound indicates that a record which was searched
+	// or queried for could not be retrieved.
+	//
+	// Use ErrNotFound to indicate the request could not be fulfilled
+	// in the case of PopulateByID and PopulateByField. For an empty query,
+	// rather, simply return an empty iterator.
 	ErrNotFound = formatError("record not found")
 
-	// ErrNoConnection is used if a DB drive has not yet
-	// connected to, or has lost connection with the DBMS
+	// ErrNoConnection indicates some sort of connection error.
+	//
+	// Use ErrNoConnection for DB implementations which traverse the network,
+	// and in the case that they fail. It should be used for all network
+	// failures.
 	ErrNoConnection = formatError("no database connection")
 
-	// ErrInvalidID is used if a Record given to a driver
-	// does not have a validly parseable id
+	// ErrInvalidID indicates that a record given to a DB has an invalid ID
+	//
+	// We need this error of the rather loose typing on the types of ID
+	// which the DB interface supports. Some databases use numerical ids whereas
+	// others use UUIDs.
+	//
+	// Use ErrInvalidID to indicate that the ID of a record handed to a DB
+	// had an ID that could not be satisfactorily parsed.
 	ErrInvalidID = formatError("invalid id")
 
-	// ErrAccessDenial is used if a driver encounters some sort
-	// of access restriction from the DBMS. It is also used if
-	// an implementation of data.Access rejects access to a Client
+	// ErrAccessDenial indicates that the DB does that have access to a particular
+	// table, or record, or query mechanism.
+	//
+	// Use ErrAccessDenial to indicate that some sort of AccessDenial was encountered.
+	// You may find it effective to wrap a data.DB in some sort of structure which
+	// maintains the access rules for you schema. In this case, said structure could
+	// use ErrAccessDenial to reject access to a client.
 	ErrAccessDenial = formatError("access denied")
 )
