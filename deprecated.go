fdf0be82d5906333b22dd9477c8c6ec10a436a9e
Nick Landolfi <nclandolfi@gmail.com>
2016-01-13 16:41:50 -0800

    remove data.Access and data.Client

--- deprecated.go
+++ deprecated.go
@@ -1,17 +1,1 @@
 package data
-
-// a (largely) unsuccesful attempt at defining Access interfaces
-// it has become clear the best way to do this is wrapping a db interface
-// in another structure which implements the db interface and therefore
-// manages access. Client is severely to restricting, you must know more
-// about your access control schema and user structure.
-type (
-	Client interface {
-		Record
-	}
-
-	Access interface {
-		DB
-		Client() Client
-	}
-)
