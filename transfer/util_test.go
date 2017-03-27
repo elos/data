47e0cecfb1faca707cd508542910b3edd9e432d9
Nick Landolfi <nclandolfi@gmail.com>
2017-03-26 22:49:02 -0700

    better tests for transfer utils

--- transfer/util_test.go
+++ transfer/util_test.go
@@ -6,15 +6,24 @@ import (
 	"github.com/elos/data/transfer"
 )
 
+type s struct {
+	Foo string
+}
+
 type T struct {
 	One string
 	Two int
+
+	Other *s
 }
 
-func TestTransferAttrs(t *testing.T) {
+func TestTransferAttrsToMap(t *testing.T) {
 	x := &T{
 		One: "one",
 		Two: 2,
+		Other: &s{
+			Foo: "yes",
+		},
 	}
 
 	m := make(map[string]interface{})
@@ -27,5 +36,51 @@ func TestTransferAttrs(t *testing.T) {
 	}
 	if got, want := one.(string), "one"; got != want {
 		t.Fatalf("one.(string): got %q, want %q", got, want)
+	}
+
+	two, ok := m["Two"]
+	if got, want := ok, true; got != want {
+		t.Fatalf("_, ok := m[\"Two\"]: got %t, want %t", got, want)
+	}
+	if got, want := two.(float64), float64(2); got != want {
+		t.Fatalf("two.(string): got %f, want %f", got, want)
+	}
+
+	other, ok := m["Other"]
+	if got, want := ok, true; got != want {
+		t.Fatalf("_, ok := m[\"Other\"]: got %t, want %t", got, want)
+	}
+	foo, ok := other.(map[string]interface{})["Foo"]
+	if got, want := ok, true; got != want {
+		t.Fatalf("_, ok := m[\"Other\"][\"Foo\"]: got %t, want %t", got, want)
+	}
+	if got, want := foo.(string), "yes"; got != want {
+		t.Fatalf("foo.(string): got %q, want %q", got, want)
+	}
+}
+
+func TestTransferAttrsToStruct(t *testing.T) {
+	from := &T{
+		One: "one",
+		Two: 2,
+		Other: &s{
+			Foo: "yes",
+		},
+	}
+
+	to := &T{}
+
+	transfer.TransferAttrs(from, to)
+
+	if got, want := to.One, "one"; got != want {
+		t.Errorf("to.One: got %q, want %q", got, want)
+	}
+
+	if got, want := to.Two, 2; got != want {
+		t.Errorf("to.Two: got %f, want %f", got, want)
+	}
+
+	if got, want := to.Other.Foo, "yes"; got != want {
+		t.Errorf("to.Other.Foo: got %q, want %q", got, want)
 	}
 }
